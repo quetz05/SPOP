a0821ced50745591daadab8a6f67347921f40cc3
Quetz <quetz05@gmail.com>
2016-01-17 16:34:19 +0100

    Refaktoryzacja typow

--- projekt.hs
+++ projekt.hs
@@ -5,9 +5,15 @@ import Data.List
 import Control.Exception 
 import System.IO.Error
 
-data Creek = Creek ((Int, Int)) [((Int, Int), Int)]  deriving (Eq, Show, Read) 
-type Point = (Int, Int)
 
+
+type Node = (Int, Int)
+type NodeWeight = (Node, Int)
+type Field = (Int, Int)
+type FieldStatus = (Field, Int)
+type Dimension = (Int, Int)
+type Selection = (Node, Field)
+data Creek = Creek Dimension [NodeWeight]  deriving (Eq, Show, Read) 
 
 main =  do
             putStrLn "Witaj w programie Strumyczek autorstwa Jarosława Kornaty i Bartosza Domagały"
@@ -36,54 +42,54 @@ readInt = read
 -- pusta lista przeciec to zwracamy pusta liste zamalowanych
 zrob (Creek (_,_) []) = []
 zrob (Creek (dimX,dimY) (((x,y), v) : tail)) = 
-    process dimX dimY  (sortBy sortListOrder (((x,y), v) : tail) ) (prepareBoard dimX dimY (((x,y), v) : tail)) []
+    process (dimX, dimY)  (sortBy sortListOrder (((x,y), v) : tail) ) (prepareBoard dimX dimY (((x,y), v) : tail)) []
 
-process :: Int -> Int -> [((Int, Int), Int)] -> [((Int, Int), Int)]  -> [((Int, Int),(Int, Int))]-> [((Int, Int),(Int, Int))]
-process _ _ [] _ wynik = wynik
-process  dimX dimY (((x,y), v) : tail) board wynik = 
+process :: Dimension -> [NodeWeight] -> [FieldStatus]  -> [Selection]-> [Selection]
+process _ [] _ wynik = wynik
+process  (dimX, dimY) (((x,y), v) : tail) board wynik = 
     if v > 0
         then 
-            process dimX dimY  (((x,y), (v-1)) : tail) board (checkField dimX dimY wynik (x,y))
+            process (dimX, dimY)  (((x,y), (v-1)) : tail) board (checkField (dimX, dimY) wynik (x,y))
     else 
-        process dimX dimY  tail board wynik
+        process (dimX, dimY)  tail board wynik
 
-outOfFields :: Int -> Int -> Int -> Int -> Bool
-outOfFields x y dimX dimY = (x < 0 || y < 0 || x > dimX || y > dimY)
+outOfFields :: Field -> Dimension -> Bool
+outOfFields (x, y) (dimX, dimY) = (x < 0 || y < 0 || x > dimX || y > dimY)
 
-containsPoint :: [(Int, Int)] -> (Int,Int) -> Bool
+containsPoint :: [Field] -> Field -> Bool
 containsPoint [] (_,_) = False
 containsPoint (x:xs) (a,b) = if(fst x == a && snd x == b) then True
                                  else containsPoint xs (a,b)
                                  
-containsPoint2 :: [((Int, Int),(Int, Int))] -> ((Int,Int),(Int,Int)) -> Bool
+containsPoint2 :: [(Node,Field)] -> (Node,Field) -> Bool
 containsPoint2 [] (_,_) = False
 containsPoint2 (x:xs) (a,b) =  if((fst(fst x)) == fst a && snd(fst x) == snd a && fst(snd x) == fst b && snd(snd x) == snd b) then True
                                            else containsPoint2 xs (a,b)
                                            
-checkField :: Int -> Int -> [((Int, Int),(Int, Int))] -> (Int, Int) -> [((Int, Int),(Int, Int))]
-checkField dimx dimy [] (a,b) = 
-    if(not (outOfFields (a-1) (b-1) dimx dimy))
+checkField :: Dimension -> [(Node,Field)] -> Node -> [(Node,Field)]
+checkField (dimX, dimY) [] (a,b) = 
+    if(not (outOfFields ((a-1), (b-1)) (dimX, dimY)))
         then [((a,b),(a-1,b-1))]
-    else if(not (outOfFields (a-1) (b) dimx dimy))
+    else if(not (outOfFields ((a-1), b) (dimX, dimY)))
         then [((a,b),(a-1,b))]
-    else if(not (outOfFields (a) (b-1) dimx dimy))
+    else if(not (outOfFields (a,(b-1)) (dimX, dimY)))
         then [((a,b),(a,b-1))]
-    else if(not (outOfFields (a) (b) dimx dimy))
+    else if(not (outOfFields (a, b) (dimX, dimY)))
         then [((a,b),(a,b))]
     else []
-checkField dimx dimy (((x1, y1),(x2,y2)):tail) (a,b) = 
-    if(not (outOfFields (a-1) (b-1) dimx dimy) && not (containsPoint2 (((x1, y1),(x2,y2)):tail) ((a,b),(a-1,b-1))))
+checkField (dimX, dimY) (((x1, y1),(x2,y2)):tail) (a,b) = 
+    if(not (outOfFields ((a-1), (b-1)) (dimX, dimY)) && not (containsPoint2 (((x1, y1),(x2,y2)):tail) ((a,b),(a-1,b-1))))
         then (((a,b),(a-1,b-1)):((x1, y1),(x2,y2)):tail)
-    else if(not (outOfFields (a-1) (b) dimx dimy) && not (containsPoint2 (((x1, y1),(x2,y2)):tail) ((a,b),(a-1,b))))
+    else if(not (outOfFields ((a-1), b) (dimX, dimY)) && not (containsPoint2 (((x1, y1),(x2,y2)):tail) ((a,b),(a-1,b))))
         then (((a,b),(a-1,b)):((x1, y1),(x2,y2)):tail)
-    else if(not (outOfFields (a) (b-1) dimx dimy) && not (containsPoint2 (((x1, y1),(x2,y2)):tail) ((a,b),(a,b-1))))
+    else if(not (outOfFields (a,(b-1)) (dimX, dimY)) && not (containsPoint2 (((x1, y1),(x2,y2)):tail) ((a,b),(a,b-1))))
         then (((a,b),(a,b-1)):((x1, y1),(x2,y2)):tail)
-    else if(not (outOfFields (a) (b) dimx dimy) && not (containsPoint2 (((x1, y1),(x2,y2)):tail) ((a,b),(a,b))))
+    else if(not (outOfFields (a, b) (dimX, dimY)) && not (containsPoint2 (((x1, y1),(x2,y2)):tail) ((a,b),(a,b))))
         then (((a,b),(a,b)):((x1, y1),(x2,y2)):tail)
     else []
     
     
-prettyPrint :: [((Int, Int),(Int, Int))] -> [(Int, Int)] -> [(Int, Int)]
+prettyPrint :: [(Node,Field)] -> [Field] -> [Field]
 prettyPrint [] result = result
 prettyPrint ((_,(c,d)):tail) result =
     -- trace ("(" ++ show c ++ "," ++ show d ++ ") :result: " ++ show result)$
