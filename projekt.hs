b2cea094100e68934e34c233207c092e3a76e6ff
Jaroslaw Kornata <jkornata@hotmail.com>
2016-01-17 13:33:48 +0100

    Sortowanie listy wejsciowej, funkcja do filtorwania wynikow

--- projekt.hs
+++ projekt.hs
@@ -1,5 +1,7 @@
 import System.IO  
 import Control.Monad
+import Debug.Trace
+import Data.List
 
 data Creek = Creek ((Int, Int)) [((Int, Int), Int)]  deriving (Eq, Show, Read) 
 data Point = Point (Int, Int)
@@ -20,22 +22,23 @@ loadFile f = do
                 let result = zrob obj
                 print result
 
--- funkcja mapująca					
+-- funkcja mapująca                    
 readInt  :: String -> Int
 readInt = read
 
+    
 -- pusta lista przeciec to zwracamy pusta liste zamalowanych
 zrob (Creek (_,_) []) = []
-zrob (Creek (dimX,dimY) (((x,y), v) : tail)) = process dimX dimY  (((x,y), v) : tail) []
+zrob (Creek (dimX,dimY) (((x,y), v) : tail)) = process dimX dimY  (sortBy sortListOrder (((x,y), v) : tail) ) []
 
 process :: Int -> Int -> [((Int, Int), Int)] -> [((Int, Int),(Int, Int))] -> [((Int, Int),(Int, Int))]
 process _ _ [] wynik = wynik
 process  dimX dimY (((x,y), v) : tail) wynik = 
     if v > 0
-		then 
-			process dimX dimY  (((x,y), (v-1)) : tail) (checkField dimX dimY wynik (x,y))
-	else 
-		process dimX dimY  tail (checkField dimX dimY wynik (x,y))
+        then 
+            process dimX dimY  (((x,y), (v-1)) : tail) (checkField dimX dimY wynik (x,y))
+    else 
+        process dimX dimY  tail wynik
 
 outOfFields :: Int -> Int -> Int -> Int -> Bool
 outOfFields x y dimX dimY = (x < 0 || y < 0 || x > dimX || y > dimY)
@@ -44,7 +47,7 @@ containsPoint :: [(Int, Int)] -> (Int,Int) -> Bool
 containsPoint [] (_,_) = False
 containsPoint (x:xs) (a,b) = if(fst x == a && snd x == b) then True
                                  else containsPoint xs (a,b)
-								 
+                                 
 containsPoint2 :: [((Int, Int),(Int, Int))] -> ((Int,Int),(Int,Int)) -> Bool
 containsPoint2 [] (_,_) = False
 containsPoint2 (x:xs) (a,b) =  if((fst(fst x)) == fst a && snd(fst x) == snd a && fst(snd x) == fst b && snd(snd x) == snd b) then True
@@ -54,24 +57,41 @@ checkField :: Int -> Int -> [((Int, Int),(Int, Int))] -> (Int, Int) -> [((Int, I
 checkField dimx dimy [] (a,b) = 
     if(not (outOfFields (a-1) (b-1) dimx dimy))
         then [((a,b),(a-1,b-1))]
-	else if(not (outOfFields (a-1) (b) dimx dimy))
+    else if(not (outOfFields (a-1) (b) dimx dimy))
         then [((a,b),(a-1,b))]
     else if(not (outOfFields (a) (b-1) dimx dimy))
         then [((a,b),(a,b-1))]
     else if(not (outOfFields (a) (b) dimx dimy))
         then [((a,b),(a,b))]
-	else []
+    else []
 checkField dimx dimy (((x1, y1),(x2,y2)):tail) (a,b) = 
     if(not (outOfFields (a-1) (b-1) dimx dimy) && not (containsPoint2 (((x1, y1),(x2,y2)):tail) ((a,b),(a-1,b-1))))
         then (((a,b),(a-1,b-1)):((x1, y1),(x2,y2)):tail)
-	else if(not (outOfFields (a-1) (b) dimx dimy) && not (containsPoint2 (((x1, y1),(x2,y2)):tail) ((a,b),(a-1,b))))
+    else if(not (outOfFields (a-1) (b) dimx dimy) && not (containsPoint2 (((x1, y1),(x2,y2)):tail) ((a,b),(a-1,b))))
         then (((a,b),(a-1,b)):((x1, y1),(x2,y2)):tail)
     else if(not (outOfFields (a) (b-1) dimx dimy) && not (containsPoint2 (((x1, y1),(x2,y2)):tail) ((a,b),(a,b-1))))
         then (((a,b),(a,b-1)):((x1, y1),(x2,y2)):tail)
     else if(not (outOfFields (a) (b) dimx dimy) && not (containsPoint2 (((x1, y1),(x2,y2)):tail) ((a,b),(a,b))))
         then (((a,b),(a,b)):((x1, y1),(x2,y2)):tail)
-	else []
+    else []
     
+    
+prettyPrint :: [((Int, Int),(Int, Int))] -> [(Int, Int)] -> [(Int, Int)]
+prettyPrint [] result = result
+prettyPrint ((_,(c,d)):tail) result =
+    -- trace ("(" ++ show c ++ "," ++ show d ++ ") :result: " ++ show result)$
+    if containsPoint result (c,d)
+        then prettyPrint tail result
+    else
+         prettyPrint tail ((c,d):result)
+        
+        
+sortListOrder (_, v1) (_, v2) = 
+    if v1 >= v2
+        then 
+             LT
+    else
+         GT
 -- // zmienne
 -- Creek (w,h) [((a1,b1), v1), ((a2, b2), v2) ... ((an, bn), vn)]
 -- x1 = ((a1,b1), v1)
